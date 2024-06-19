153d65a5e08dcf6159617367fa918f879591d48c
Natalia Salukvadze <nsalukvadzee@gmail.com>
2024-06-19 21:33:52 +0400

    add task print tasks

--- src/Main.java
+++ src/Main.java
@@ -66,9 +66,14 @@ public class Main {
 
     }
 
-    //todo დააბრუნოს სიიდან სახელის მიხედვით
     private static void getTask(Scanner scanner) {
-
+        System.out.print("enter task name: ");
+        String taskName = scanner.nextLine();
+        if(taskMap.containsKey(taskName)){
+            taskMap.get(taskName).print();
+        }else {
+            System.out.println("ასეთი სახელით ტასკი არ არსებობს\n");
+        }
     }
 
     private static void deleteTask(Scanner scanner) {
@@ -82,14 +87,49 @@ public class Main {
         }
     }
 
-    //todo დააბრუნოს მთლიანი სია
     private static void getTaskList() {
-
+        if(taskMap == null || taskMap.isEmpty())
+        {
+            System.out.println("ტასკების სია ცარიელია\n");
+        }
+        else
+        {
+            for (Map.Entry<String, GeneralTask> me :
+                    taskMap.entrySet()) {
+                System.out.print(me.getKey() + "\n");
+                //me.getValue().print();
+            }
+        }
     }
 
-    //todo შექმნას ახალი
     private static void createTask(Scanner scanner) {
+        System.out.print("enter task type (LimitedTimeTask, RepeateableTask, BasicTask: ");
+        String taskType = scanner.nextLine();
 
+        System.out.print("enter task name: ");
+        String taskName = scanner.nextLine();
+        switch (taskType)
+        {
+            case "LimitedTimeTask":
+                System.out.print("enter task date: ");
+                String taskDateString = scanner.nextLine();
+                LocalDateTime taskDate = LocalDateTime.parse(taskDateString);
+                taskMap.put(taskName, new LimitedTimeTask( taskName, userName, taskDate));
+                System.out.print("task created \n");
+                break;
+            case "RepeateableTask":
+                System.out.print("enter task execute count: ");
+                int taskExecuteCount = Integer.parseInt(scanner.nextLine());
+                taskMap.put(taskName, new RepeatableTask( taskName, userName, taskExecuteCount));
+                System.out.print("task created \n");
+                break;
+            case "BasicTask":
+                taskMap.put(taskName, new GeneralTask( taskName, userName));
+                System.out.print("task created \n");
+                break;
+            default:
+                System.out.println("task type is unknown");
+        }
     }
 
     public static void authenticate(Scanner scanner){
@@ -131,8 +171,5 @@ public class Main {
 
         System.out.printf("task updated\n" );
         generalTask.print();
-
-
-
     }
 }
